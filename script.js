7c7840f724569a6f86282dfd30ec8bcba260608a
Tran Thi Thu Trang <thutrang1232003@gmail.com>
2022-10-31 10:58:36 +0200

    Define JS function bingo

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let name_1 = myName('Trang');
 console.log(name_1);
 
 
+const canGetPrize = (number) => {
+    if (number > 5) {
+        return 'Bingo'
+    } 
+}
+
+let number_1 = canGetPrize(10);
+console.log(number_1);
+
+let number_2 = canGetPrize(4);
+console.log(number_2);
